0411ddff02399170e06c5c531f53389fe9a4dfda
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-04-07 17:40:16 +0200

    horoscope - docker deploy ready with docker-compose

---
+++ ppb-product-horoscope/src/main/java/com/digital/payments/product/PpbProductHoroscopeApplication.java
@@ -0,0 +1,12 @@
+package com.digital.payments.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpbProductHoroscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpbProductHoroscopeApplication.class, args);
+	}
+}
---
+++ ppb-product-horoscope/src/main/java/com/digital/payments/product/controller/HoroscopeController.java
@@ -0,0 +1,16 @@
+package com.digital.payments.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/horoscope")
+public class HoroscopeController {
+
+	@GetMapping
+	public String index(Model model) {
+		return "index";
+	}
+}
