02f26ada4f639c462f5fbc9eca65d1067f1193e7
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-04-07 13:19:54 +0200

    first commit with horoscope module

---
+++ src/main/java/com/digital/payments/product/PpbProductParentApplication.java
@@ -0,0 +1,13 @@
+package com.digital.payments.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpbProductParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpbProductParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/payments/product/PpbProductParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.payments.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpbProductParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
