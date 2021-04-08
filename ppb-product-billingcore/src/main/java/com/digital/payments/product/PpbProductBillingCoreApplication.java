149ce99f93756050aabc9382c526ec6c8f3666f7
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-04-08 10:20:37 +0200

    billingcore - new project for manage the integration with payment methods

---
+++ ppb-product-billingcore/src/main/java/com/digital/payments/product/PpbProductBillingCoreApplication.java
@@ -0,0 +1,12 @@
+package com.digital.payments.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpbProductBillingCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpbProductBillingCoreApplication.class, args);
+	}
+}
