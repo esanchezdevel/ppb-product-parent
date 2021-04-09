c2ed3a61a3ea6ee56650827826b4a74bf3aa3fb3
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-04-09 13:26:45 +0200

    horoscope - make subscribe request from frontend. fix static resource paths. docker - add container names

--- ppb-product-horoscope/src/main/java/com/digital/payments/product/controller/HoroscopeController.java
+++ ppb-product-horoscope/src/main/java/com/digital/payments/product/controller/HoroscopeController.java
@@ -1,9 +1,12 @@
 package com.digital.payments.product.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,7 +22,18 @@ public class HoroscopeController {
 	public String landingPage(@PathVariable("sign") String sign, Model model) {
 		
 		model.addAttribute("sign", sign);
+		model.addAttribute("productTransactionId", 1111111);
 		
 		return "landingPage";
+	}
+	
+	@PostMapping("/subscribe")
+	public ResponseEntity<?> subscribe(@RequestBody String body, Model model) {
+		
+		System.out.println("TEST-subscribe request: " + body);
+		//TODO make request to billingCore
+		
+		//Redirect to product
+		return ResponseEntity.ok("{\"status\":\"ok\"}");
 	}
 }
