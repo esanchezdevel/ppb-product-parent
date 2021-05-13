17ae5b887b485cf01930a9760cf35bb5b08b10d8
Enrique Sanchez <esanchez.devel@gmail.com>
2021-05-13 20:08:31 +0200

    user-management - start creating subscription model

---
+++ ppb-product-user-management/src/main/java/com/digital/payments/product/model/Subscription.java
@@ -0,0 +1,10 @@
+package com.digital.payments.product.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "subscriptions")
+public class Subscription {
+
+}
