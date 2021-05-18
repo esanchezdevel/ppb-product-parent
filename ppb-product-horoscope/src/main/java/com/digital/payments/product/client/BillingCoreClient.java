a95ba1d24d32bee2944d3c9262a8a3b14cb1c104
Enrique Sanchez <esanchez.devel@gmail.com>
2021-05-18 19:16:53 +0200

    horoscope - feign client use url configured in properties

--- ppb-product-horoscope/src/main/java/com/digital/payments/product/client/BillingCoreClient.java
+++ ppb-product-horoscope/src/main/java/com/digital/payments/product/client/BillingCoreClient.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.digital.payments.product.dto.SubscribeRequest;
 import com.digital.payments.product.dto.SubscribeResponse;
 
-//@FeignClient(name = "billingcore", url = "http://ppb-product-billingcore:8080")
-@FeignClient(name = "billingcore", url = "http://localhost:8080")
+@FeignClient(name = "billingcore", url = "${billingcore.url}")
 public interface BillingCoreClient {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/subscribe")
