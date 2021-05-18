06a45890629d20f57ec84c77820f0221fb67cb97
Enrique Sanchez <esanchez.devel@gmail.com>
2021-05-18 19:32:44 +0200

    horoscope - modify subscribeService test

--- ppb-product-horoscope/src/test/java/com/digital/payments/product/service/SubscribeServiceTest.java
+++ ppb-product-horoscope/src/test/java/com/digital/payments/product/service/SubscribeServiceTest.java
@@ -1,19 +1,33 @@
 package com.digital.payments.product.service;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.digital.payments.product.client.BillingCoreClient;
+import com.digital.payments.product.dto.SubscribeRequest;
+import com.digital.payments.product.dto.SubscribeResponse;
 import com.digital.payments.product.jpa.entity.Transaction;
+import com.digital.payments.product.jpa.repository.TransactionRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class SubscribeServiceTest {
 
 	private static Transaction transaction;
+	
+	@Mock
+	private BillingCoreClient billingCoreClient;
+	
+	@Mock
+	private TransactionRepository transactionRepository;
 	
 	@InjectMocks
     private SubscribeService subscribe;
@@ -30,8 +44,23 @@ public class SubscribeServiceTest {
 	@DisplayName("test_subscribe_success")
 	@Disabled
 	void testSubscribeSuccess() {
+	
+		SubscribeRequest subscribeRequest = new SubscribeRequest();		
+		subscribeRequest.setProduct("horoscope");
+		subscribeRequest.setSign("tauro");
+		subscribeRequest.setProductTransactionId(1L);
+		subscribeRequest.setSubscriptionId("11111");
 		
-
+		SubscribeResponse subscribeResponse = new SubscribeResponse();
+		subscribeResponse.setTransactionStatus("SUBSCRIBED");
+		
+		when(transactionRepository.getOne(anyLong())).thenReturn(transaction);
+		when(billingCoreClient.subscribe(subscribeRequest)).thenReturn(subscribeResponse);
+		
+		SubscribeResponse response = subscribe.execute(subscribeRequest);
+		
+		assertNotNull(response);
+		assertEquals("SUBSCRIBED", response.getTransactionStatus());
 	}
 	
 	
@@ -39,6 +68,21 @@ public class SubscribeServiceTest {
 	@DisplayName("test_subscribe_error")
 	@Disabled
 	void testSubscribeError() {
+		SubscribeRequest subscribeRequest = new SubscribeRequest();		
+		subscribeRequest.setProduct("horoscope");
+		subscribeRequest.setSign("tauro");
+		subscribeRequest.setProductTransactionId(1L);
+		subscribeRequest.setSubscriptionId("11111");
 		
+		SubscribeResponse subscribeResponse = new SubscribeResponse();
+		subscribeResponse.setTransactionStatus("ERROR");
+		
+		when(transactionRepository.getOne(anyLong())).thenReturn(transaction);
+		when(billingCoreClient.subscribe(subscribeRequest)).thenReturn(subscribeResponse);
+		
+		SubscribeResponse response = subscribe.execute(subscribeRequest);
+		
+		assertNotNull(response);
+		assertEquals("ERROR", response.getTransactionStatus());
 	}
 }
