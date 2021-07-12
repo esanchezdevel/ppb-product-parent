00fc4204d06d06d78f5e296c4bd1ef757a4f77a0
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-07-12 15:24:35 +0200

    horoscope - add a unit test of landingPage in controller

--- ppb-product-horoscope/src/test/java/com/digital/payments/product/controller/HoroscopeControllerTest.java
+++ ppb-product-horoscope/src/test/java/com/digital/payments/product/controller/HoroscopeControllerTest.java
@@ -1,5 +1,6 @@
 package com.digital.payments.product.controller;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -13,7 +14,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.digital.payments.product.dto.SubscribeResponse;
 import com.digital.payments.product.interceptor.StatsInterceptor;
+import com.digital.payments.product.jpa.entity.Transaction;
 import com.digital.payments.product.jpa.repository.TransactionRepository;
 import com.digital.payments.product.service.HoroscopeService;
 import com.digital.payments.product.service.SubscribeService;
@@ -21,6 +24,8 @@ import com.digital.payments.product.service.SubscribeService;
 @WebMvcTest
 public class HoroscopeControllerTest {
 
+	private static final String CONTENT_TYPE_HTML = MediaType.TEXT_HTML_VALUE + ";charset=UTF-8";
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
@@ -42,11 +47,27 @@ public class HoroscopeControllerTest {
 		
 		when(statsInterceptor.preHandle(any(), any(), any())).thenReturn(true);
 		
-		String contentType = MediaType.TEXT_HTML_VALUE + ";charset=UTF-8";
 		
-		mockMvc.perform(get("/horoscope").contentType(contentType))
+		
+		mockMvc.perform(get("/horoscope").contentType(CONTENT_TYPE_HTML))
 		.andExpect(status().isOk())
-		.andExpect(content().contentType(contentType));
+		.andExpect(content().contentType(CONTENT_TYPE_HTML));
+	}
+	
+	@Test
+	@DisplayName("test_landing_page")
+	void testLandingPage() throws Exception {
+		
+		when(statsInterceptor.preHandle(any(), any(), any())).thenReturn(true);
+		
+		Transaction transaction = new Transaction();
+		transaction.setId(1L);
+		when(transactionRepository.save(any())).thenReturn(transaction);
+		
+		mockMvc.perform(get("/horoscope/lp/Leo").contentType(CONTENT_TYPE_HTML))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType(CONTENT_TYPE_HTML))
+		.andExpect(content().string(containsString("Leo")));
 	}
 	
 }
